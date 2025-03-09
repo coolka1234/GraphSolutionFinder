05e371be8c8011fa2ee7f843a385f46bed8e6e4e
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2025-03-09 13:32:02 +0100

    djikstra works, optimizing output

--- src/djikstra.py
+++ src/djikstra.py
@@ -1,5 +1,9 @@
 # Description: Djikstra's algorithm for finding the shortest path in a graph
+from itertools import count
 import networkx as nx
+
+from collections import deque
+from heapq import heappop, heappush
 class Djikstra():
     def __init__(self, graph: nx.Graph):
         self.graph = graph
@@ -28,8 +32,54 @@ class Djikstra():
                 min_node = node
         return min_node
 
+    @staticmethod
+    def multi_source_dijkstra(G, sources, pred=None, paths=None, target=None, cutoff=None, weight="weight"):
+        G_succ = G._adj  
+        push = heappush
+        pop = heappop
+        dist = {}  
+        seen = {}
+        c = count()
+        fringe = []
+        for source in sources:
+            seen[source] = 0
+            push(fringe, (0, next(c), source))
+        while fringe:
+            (d, _, v) = pop(fringe)
+            if v in dist:
+                continue  
+            dist[v] = d
+            if v == target:
+                break
+            for u, e in G_succ[v].items():
+                cost = weight(v, u, e)
+                if cost is None:
+                    continue
+                vu_dist = dist[v] + cost
+                if cutoff is not None:
+                    if vu_dist > cutoff:
+                        continue
+                if u in dist:
+                    u_dist = dist[u]
+                    if vu_dist < u_dist:
+                        raise ValueError("Contradictory paths found:", "negative weights?")
+                    elif pred is not None and vu_dist == u_dist:
+                        pred[u].append(v)
+                elif u not in seen or vu_dist < seen[u]:
+                    seen[u] = vu_dist
+                    push(fringe, (vu_dist, next(c), u))
+                    if paths is not None:
+                        paths[u] = paths[v] + [u]
+                    if pred is not None:
+                        pred[u] = [v]
+                elif vu_dist == seen[u]:
+                    if pred is not None:
+                        pred[u].append(v)
+        return dist
+
 if __name__=='__main__':
-    from process_csv import read_and_return
-    G = read_and_return()
+    from process_csv import read_and_return, df_test
+    G = read_and_return(df_test)
     djikstra = Djikstra(G)
-    print(djikstra.shortest_path('Kolumba', 'Tczewska'))
+    # print(djikstra.multi_source_dijkstra(G,'Czajkowskiego', target='Krucza'))
+    print(djikstra.shortest_path('Czajkowskiego', 'Krucza'))
